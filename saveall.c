60c8226dd11498a0b4a4e6dbfe1ba8a2cac23492
chengjf <sharpkingdom@gmail.com>
2016-01-18 18:48:57 +0800

    Delete show-message when save file.

--- saveall.c
+++ saveall.c
@@ -1,7 +1,5 @@
-/* Replace "dll.h" with the name of your header */
 #include "saveall.h"
 #include <windows.h>
-
 
 // Description of this Plug-In (as displayed in Plug-In configuration dialog)
 char *const Desc = "Save All Plugin v0.1";
@@ -29,7 +27,7 @@ BOOL WINAPI DllMain(HINSTANCE hinstDLL,DWORD fdwReason,LPVOID lpvReserved)
 			break;
 		}
 	}
-	
+
 	/* Return TRUE on success, FALSE on failure */
 	return TRUE;
 }
@@ -57,12 +55,12 @@ char* (*SYS_OracleHome)();
 char* (*SYS_RootDir)();
 void  (*IDE_MenuState)(int ID, int Index, BOOL Enabled);
 void  (*IDE_GetBrowserInfo)(char **ObjectType, char **ObjectOwner, char **ObjectName);
-int   (*SQL_Execute)(char *SQL);
+int	  (*SQL_Execute)(char *SQL);
 BOOL  (*SQL_Eof)();
-int   (*SQL_Next)();
+int	  (*SQL_Next)();
 char* (*SQL_Field)(int Field);
 char* (*IDE_Filename)();
-int   (*IDE_GetWindowCount)();
+int	  (*IDE_GetWindowCount)();
 BOOL  (*IDE_SelectWindow)(int Index);
 BOOL  (*IDE_CanSaveWindow)();
 BOOL  (*IDE_SaveFile)();
@@ -74,39 +72,39 @@ void RegisterCallback(int Index, void *Addr)
 {
   switch (Index)
   {
-    case 2 :
-      memcpy(&SYS_Registry, &Addr, sizeof(Addr));
-      break;
-    case 3 :
-      memcpy(&SYS_RootDir, &Addr, sizeof(Addr));
-      break;
-    case 4 :
-      memcpy(&SYS_OracleHome, &Addr, sizeof(Addr));
-      break;
-    case 10 :
-    	memcpy(&IDE_MenuState, &Addr, sizeof(Addr));
-      break;
-    case 13 :
-    	memcpy(&IDE_Filename, &Addr, sizeof(Addr));
-      break;
-    case 22 :
-    	memcpy(&IDE_SaveFile, &Addr, sizeof(Addr));
-      break;
-    case 23 :
-    	memcpy(&IDE_Filename, &Addr, sizeof(Addr));
-      break;
-    case 29 :
-    	memcpy(&IDE_SetFilename, &Addr, sizeof(Addr));
-      break;
-    case 80 :
-    	memcpy(&IDE_GetWindowCount, &Addr, sizeof(Addr));
-      break;
-    case 81 :
-    	memcpy(&IDE_SelectWindow, &Addr, sizeof(Addr));
-      break;
-    case 101 :
-    	memcpy(&IDE_CanSaveWindow, &Addr, sizeof(Addr));
-      break;
+	case 2 :
+	  memcpy(&SYS_Registry, &Addr, sizeof(Addr));
+	  break;
+	case 3 :
+	  memcpy(&SYS_RootDir, &Addr, sizeof(Addr));
+	  break;
+	case 4 :
+	  memcpy(&SYS_OracleHome, &Addr, sizeof(Addr));
+	  break;
+	case 10 :
+		memcpy(&IDE_MenuState, &Addr, sizeof(Addr));
+	  break;
+	case 13 :
+		memcpy(&IDE_Filename, &Addr, sizeof(Addr));
+	  break;
+	case 22 :
+		memcpy(&IDE_SaveFile, &Addr, sizeof(Addr));
+	  break;
+	case 23 :
+		memcpy(&IDE_Filename, &Addr, sizeof(Addr));
+	  break;
+	case 29 :
+		memcpy(&IDE_SetFilename, &Addr, sizeof(Addr));
+	  break;
+	case 80 :
+		memcpy(&IDE_GetWindowCount, &Addr, sizeof(Addr));
+	  break;
+	case 81 :
+		memcpy(&IDE_SelectWindow, &Addr, sizeof(Addr));
+	  break;
+	case 101 :
+		memcpy(&IDE_CanSaveWindow, &Addr, sizeof(Addr));
+	  break;
   }
 }
 
@@ -124,7 +122,7 @@ char* CreateMenuItem(int Index)
 {
   switch (Index)
   {
-    case 1 : return "Plugins / &Save All";
+	case 1 : return "Plugins / &Save All";
   }
   return "";
 }
@@ -134,29 +132,29 @@ void OnMenuClick(int Index)
 {
   switch (Index)
   {
-    case 1 :{
-      SaveAll();
-      break;
+	case 1 :{
+	  SaveAll();
+	  break;
 	  }
   }
 }
 //---------------------------------------------------------------------------
 
 char* About(){
-	return "SaveAllPlugin v0.1\n2016:chengjianfeng\nEmail:sharpkingdom@gmail.com\n\nBest wishes to you!"; 
+	return "SaveAllPlugin v0.1\n2016:chengjianfeng\nEmail:sharpkingdom@gmail.com\n\nBest wishes to you!";
 }
 
 void  Configure()
 {
 	char* root = (*SYS_RootDir)();
 	char msg[200];
-    msg[0] = 0;
-    strcat(msg, "Please modify config file:");
-    strcat(msg, root);
-    strcat(msg, "\\PlugIns\\");
-    strcat(msg, "saveall.ini\n");
-    strcat(msg, "current basedir is ");
-    strcat(msg, basedir);
+	msg[0] = 0;
+	strcat(msg, "Please modify config file:");
+	strcat(msg, root);
+	strcat(msg, "\\PlugIns\\");
+	strcat(msg, "saveall.ini\n");
+	strcat(msg, "current basedir is ");
+	strcat(msg, basedir);
 	ShowMessage(msg);
 }
 
@@ -169,27 +167,26 @@ void load_config(){
 	char msg[100];
 	msg[0] = 0;
 
-    /* ini_r is a structure that holds state to make this reader
-     * reentrant */
+	/* ini_r is a structure that holds state to make this reader
+	 * reentrant */
 	struct read_ini *ini_r = NULL;
 
-    /* "test.ini" will be parsed into "ini" */
-    
-    char* root = (*SYS_RootDir)();
-    char filename[100];
-    filename[0] = 0;
-    strcat(filename, root);
-    strcat(filename, "\\PlugIns\\");
-    strcat(filename, "saveall.ini");
-//    ShowMessage(filename);
+	/* "test.ini" will be parsed into "ini" */
+
+	char* root = (*SYS_RootDir)();
+	char filename[100];
+	filename[0] = 0;
+	strcat(filename, root);
+	strcat(filename, "\\PlugIns\\");
+	strcat(filename, "saveall.ini");
 	struct ini *ini = read_ini(&ini_r, filename);
 
-    /* pretty printing of ini structure */
+	/* pretty printing of ini structure */
 	ini_pp(ini);
 
-    /* retrieve a value */
-    value = ini_get_value(ini, "section", "basedir");
-    if(value != NULL){
+	/* retrieve a value */
+	value = ini_get_value(ini, "section", "basedir");
+	if(value != NULL){
 		strcat(msg, "value is\n");
 		strcat(msg, value);
 		strcpy(basedir, value);
@@ -197,11 +194,10 @@ void load_config(){
 	}else{
 //		ShowMessage("cannot get key");
 	}
-   
 
-    /* free memory */
+	/* free memory */
 	destroy_ini(ini);
-    cleanup_readini(ini_r);
+	cleanup_readini(ini_r);
 }
 
 void SaveAll(){
@@ -213,21 +209,18 @@ void SaveAll(){
 			if(strcmp(fileName,"")==0){
 				SaveFile(cnt);
 			}else{
-				ShowMessage(fileName);
 				if(IDE_CanSaveWindow()){
 					if(IDE_SaveFile()){
 						//IDE_CloseFile();
 					}
 				}else{
-					ShowMessage("Can not save");	
+					ShowMessage("Can not save");
 				}
 			}
 		}else{
-			ShowMessage("selectwindow error");		
+			ShowMessage("selectwindow error");
 		}
-			
-	}	
-	
+	}
 }
 
 void SaveFile(int number){
@@ -241,7 +234,7 @@ void SaveFile(int number){
 	sprintf(date, "%u", p.date);
 	sprintf(time, "%u", p.time);
 	sprintf(num, "%d", number);
-	
+
 	char filename[100];
 	filename[0] = 0;
 	strcat(filename, basedir);
@@ -258,7 +251,7 @@ void SaveFile(int number){
 	strcat(filename, "_");
 	strcat(filename, num);
 	strcat(filename, ".sql");
-    
+
 
 	IDE_SetFilename(filename);
 	if(IDE_CanSaveWindow()){
@@ -266,26 +259,26 @@ void SaveFile(int number){
 			//IDE_CloseFile();
 		}
 	}else{
-		ShowMessage("Can not save");	
+		ShowMessage("Can not save");
 	}
 }
 
 void GetDate(struct datetime* datetime){
-	
+
 	time_t timep;
-    struct tm *p;
-    time(&timep);
-    p = gmtime(&timep);
-    int year = 1900+p->tm_year;
-    int month = 1+p->tm_mon;
-    int mday = p->tm_mday;
-    
-    int hour = (p->tm_hour + 8)%24;
-    int minute = p->tm_min;
-    int second = p->tm_sec;
-    
+	struct tm *p;
+	time(&timep);
+	p = gmtime(&timep);
+	int year = 1900+p->tm_year;
+	int month = 1+p->tm_mon;
+	int mday = p->tm_mday;
+
+	int hour = (p->tm_hour + 8)%24;
+	int minute = p->tm_min;
+	int second = p->tm_sec;
+
 	long date = year*10000+month*100+mday;
-    long time = hour*10000+minute*100+second;
-   	datetime->date = date;
-   	datetime->time = time;
+	long time = hour*10000+minute*100+second;
+	datetime->date = date;
+	datetime->time = time;
 }
